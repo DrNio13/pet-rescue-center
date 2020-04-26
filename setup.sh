b8140d0062ce9f5ef53dd758ce1e384d3d9c2ef3
george <georgios.perivolarakis@gamelounge.com>
2020-04-26 22:13:40 +0200

    fix: check if database is none

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_PATH="postgres://{}/{}".format('localhost:5432', "petrescuecenter")
+DATABASE_PATH="postgres://{}/{}".format('localhost:5432', "petrescuecenter")
 AUTH0_DOMAIN = 'drnio13.eu.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'pets'
